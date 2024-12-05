bd72148d12da4ac07b4ec239c2cb4bc92651d69f
baxodirovfarxod <baxodirovfarxod@2007.com>
2024-12-05 08:49:25 +0500

    Updated

--- 2.1Dars/Program.cs
+++ 2.1Dars/Program.cs
@@ -7,15 +7,7 @@ namespace _2._1Dars
     {
         static void Main(string[] args)
         {
-            var sb = new MyStringBuilder();
-            sb.Append("Farxod");
-            Console.WriteLine(sb.Display());
-            var sb2 = new MyStringBuilder("Robiya");
-            Console.WriteLine(sb2.Display());
-            sb.Append(" + " + sb2.Display());
-            Console.WriteLine(sb.Display());
-            sb2.Insert(3, "Xadicha");
-            Console.WriteLine(sb2.Display());
+            
         }
     }
 }
